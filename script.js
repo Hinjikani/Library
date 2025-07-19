5f350254f9db8c2842c8a96b983536534de4df11
Hinjikani <pascalhanif@gmail.com>
2025-07-19 22:32:30 +0700

    Add add book form

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 library = []
 
+var dialog = document.querySelector("dialog")
+var output = document.querySelector("output")
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let status = document.querySelector("#status");
+
 function Book(name, author, pages, status) {
     this.id = crypto.randomUUID()
     this.bookName = name;
@@ -13,3 +20,16 @@ function addBookToLibrary(name, author, pages, status) {
     library.push(obj)
 }
 
+function addBookPopup() {
+    dialog.showModal();
+}
+
+function showOutput() {
+    let bookTitle = title.value;
+    let bookAuthor = author.value;
+    let bookPages = pages.value;
+    let bookStatus = status.value;
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookStatus);
+    console.log("Book added! Library now has:", library.length, "books");
+    console.log("Current library:", library);
+}
